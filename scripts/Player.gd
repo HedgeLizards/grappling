3722a2f64d4ad61ffbfeacaa1eb47bf78ecd3c71
troido <troido@protonmail.com>
2022-09-16 22:31:39 +0200

    player moving around

---
+++ scripts/Player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var speed = 100;
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	var control = int(Input.is_action_pressed('right')) - int(Input.is_action_pressed('left'))
+	rotation += control * delta * PI
+	if Input.is_action_pressed("forwards"):
+		move_local_y(-delta * speed)
