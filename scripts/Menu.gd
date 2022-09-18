d0d26e7a66db822f9a7a2abe8a8df408100f2654
Julian Vos <julianvosje@gmail.com>
2022-09-18 11:39:55 +0200

    Add Stats.ships_destroyed

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,4 +1,10 @@
 extends VBoxContainer
 
+func _ready():
+	if Stats.ships_destroyed != null:
+		$Label.align = ALIGN_CENTER
+		$Label.text = "You destroyed %d ships!" % Stats.ships_destroyed
+		$Button.text = "Retry"
+
 func _on_Button_pressed():
 	get_tree().change_scene_to(preload("res://scenes/Main.tscn"))
---
+++ scripts/Stats.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var ships_destroyed
